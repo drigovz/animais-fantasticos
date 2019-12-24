9d2ebf170ccbfa2b159722850f89dec67409aeda
drigovz <rodrigodp2014@gmail.com>
2019-12-23 22:55:34 -0300

    adicionando e praticando com javascritp

--- script.js
+++ script.js
@@ -1,83 +1,42 @@
-/* const menu = document.querySelector('.menu');
-//const propriedadesMenu = menu.getBoundingClientRect();
+// Navegação por tabs
 
-// se o menu sair da tela de vizualização do usuário ele fica fixo no topo  
-window.addEventListener('scroll', () => {
-    const propriedadesMenu = menu.getBoundingClientRect();
+/**
+ * uma boa prática para não se perder na hora de manipular elementos HTML pelo Javascritp é utilizando 
+ * classes css específicas para a realização dessa manipulação. Para essas classes é interessante 
+ * utilizar o prefixo js nas classes para identificar as classes que são manipuladas via JS
+ *
+ * Uma outra forma de realizar essa manipulação é utilizando os atributos/propriedades data- no HTML.
+ * */
 
-    if (propriedadesMenu.top < 0) {
-        menu.classList.add('menu-fixo');
-    } 
-}); */
+// primeiro selecionamos cada li dentro da nossa lista
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-// somar a largura de todas as imagens da página 
-/* function somaImagens() {
-    const imgs = document.querySelectorAll('img');
-    let soma = 0;
-    imgs.forEach((item) => {
-        soma += item.offsetWidth;
-    });
-    console.log(soma);
-}
+/* verificamos se os itens acima existem na página, para caso eles não existam não executar nada e não dar erro 
+no console, então só executa se os nodeLists acima forem maiores que 0 */
+if (tabMenu.length > 0 && tabContent.length > 0) {
+    /* setamos o primeiro item do tabcontent como ativo para que quando a página carregar 
+       ele iniciar ativo e aparecendo na tela, pois se não setarmos esse primeiro item como ativo
+       nenhum item apareceria de inicio
+    */
+    tabContent[0].classList.add('ativo');
 
-window.onload = function () {
-    somaImagens();
-} */
-
-// trabalhando com eventos de clipboard
-/*const h2 = document.querySelector('h2');
-
-h2.addEventListener('copy', () => {
-	console.log(h2.innerText);
-});*/
-
-
-// pegando a tecla que o usuário digitou
-
-/* window.addEventListener('keydown', (event) => {
-    if (event.key === 'a') {
-        console.log('você teclou em A');
-    } else if (event.key === 'b') {
-        console.log('você teclou em B');
-    }
-}); */
-
-
-// adiciona a class ativo a um dos links e remove dos outros 
-const linksInternos = document.querySelectorAll('a[href^="#"]');
-
-linksInternos.forEach((link) => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        
-        linksInternos.forEach((links) => {
-            links.classList.remove('ativo');
+    // função que vai adicionar uma classe para tornar ativo as sections que possuem o conteúdo para ser exibido 
+    function activeTab(index) {
+        // remove a classe ativo de todos os outros li do tabContent
+        tabContent.forEach((section) => {
+            section.classList.remove('ativo');
         });
 
-        link.classList.toggle('ativo');
+        // adicionando a classe ativo a um item do tabContent
+        tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            // chama a função passando o index de cada item do loop como argumento para a função activeTab
+            activeTab(index);
+        });
     });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
